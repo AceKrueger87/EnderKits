e44a7226a9c1cc0d7cd3286867ab6a51e0f1e676
AceKrueger87 <99579966+AceKrueger87@users.noreply.github.com>
2023-10-11 02:08:13 -0700

    Update VIPCommand.php

--- src/Terpz710/EnderKits/Command/VIPCommand.php
+++ src/Terpz710/EnderKits/Command/VIPCommand.php
@@ -1,24 +1,22 @@
 <?php
 
-namespace Terpz710\TNTBlastRadius\Command;
+namespace Terpz710\EnderKits\Command;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginOwned;
 use pocketmine\plugin\Plugin;
-use Terpz710\TNTBlastRadius\Main;
 
-class TNTCommand extends Command implements PluginOwned {
+class VIPCommand extends Command implements PluginOwned {
 
     /** @var Plugin */
     private $plugin;
 
     public function __construct(Plugin $plugin) {
-        parent::__construct("tntradius", "Adjust the TNT blast radius");
+        parent::__construct("VIPKITS710", "Permission");
         $this->plugin = $plugin;
-        $this->setPermission("tntblastradius.cmd");
-        $this->setAliases(["tntedit"]);
+        $this->setPermission("enderkits.vip");
     }
 
     public function getOwningPlugin(): Plugin {
@@ -27,9 +25,7 @@ class TNTCommand extends Command implements PluginOwned {
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if ($sender instanceof Player) {
-            if ($this->plugin instanceof Main) {
-                $main = $this->plugin;
-                $main->openRadiusSelectorUI($sender);
+             // NOOP
             }
         } else {
             $sender->sendMessage("This command can only be used in-game.");
