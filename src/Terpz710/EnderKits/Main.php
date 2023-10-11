d8ff1d0edba5f25ef89c1c299cb26c32a78afabf
AceKrueger87 <99579966+AceKrueger87@users.noreply.github.com>
2023-10-11 01:18:40 -0700

    Update Main.php

--- src/Terpz710/EnderKits/Main.php
+++ src/Terpz710/EnderKits/Main.php
@@ -6,11 +6,13 @@ namespace Terpz710\EnderKits;
 
 use pocketmine\plugin\PluginBase;
 use Terpz710\EnderKits\Command\KitCommand;
+use Terpz710\EnderKits\Command\KitsCommand;
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
         $this->getServer()->getCommandMap()->register("kit", new Command\KitCommand($this));
+        $this->getServer()->getCommandMap()->register("kits", new Command\KitsCommand($this));
         $this->saveResource("kits.yml");
     }
 }
