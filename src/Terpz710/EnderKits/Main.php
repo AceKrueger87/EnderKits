5556217f370fe4d3813dc1038ddb8f647efa631d
AceKrueger87 <99579966+AceKrueger87@users.noreply.github.com>
2023-10-10 19:42:39 -0700

    Update Main.php

--- src/Terpz710/EnderKits/Main.php
+++ src/Terpz710/EnderKits/Main.php
@@ -6,14 +6,14 @@ namespace Terpz710\EnderKits;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-use pocketmine\scheduler\TaskHandler;
-use Terpz710\EnderKits\Command\KitCommand;
 use Terpz710\EnderKits\Task\CoolDownTask;
 
 class Main extends PluginBase {
 
+    private $coolDownTaskHandler;
+
     public function onEnable(): void {
         $this->getServer()->getCommandMap()->register("kit", new KitCommand($this, new Config($this->getDataFolder() . "kits.yml", Config::YAML)));
-        $this->coolDownTaskHandler = $this->getScheduler()->scheduleRepeatingTask(new CoolDownTask($this), 20 * 60);
+        $this->coolDownTaskHandler = $this->getScheduler()->scheduleRepeatingTask(new CoolDownTask($this, new Config($this->getDataFolder() . "kits.yml", Config::YAML)), 20 * 60);
     }
 }
