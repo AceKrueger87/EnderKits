0a7e61b0813a04c52b47bf72f0028dd4f6af3330
AceKrueger87 <99579966+AceKrueger87@users.noreply.github.com>
2023-10-10 19:37:46 -0700

    Add files via upload

---
+++ src/Terpz710/EnderKits/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Terpz710\EnderKits;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\scheduler\TaskHandler;
+use Terpz710\EnderKits\Command\KitCommand;
+use Terpz710\EnderKits\Task\CoolDownTask;
+
+class Main extends PluginBase {
+
+    /** @var TaskHandler|null */
+    private $coolDownTaskHandler;
+
+    public function onEnable(): void {
+        $this->getServer()->getCommandMap()->register("kit", new KitCommand($this, new Config($this->getDataFolder() . "kits.yml", Config::YAML)));
+        $this->coolDownTaskHandler = $this->getScheduler()->scheduleRepeatingTask(new CoolDownTask($this), 20 * 60);
+    }
+
+    public function onDisable(): void {
+        if ($this->coolDownTaskHandler !== null) {
+            $this->getScheduler()->cancelTask($this->coolDownTaskHandler->getTaskId());
+        }
+    }
+}
