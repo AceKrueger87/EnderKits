4bd0bf0060af1e417f443ecc39912dc888816030
AceKrueger87 <99579966+AceKrueger87@users.noreply.github.com>
2023-10-10 23:58:58 -0700

    Update Main.php

--- src/Terpz710/EnderKits/Main.php
+++ src/Terpz710/EnderKits/Main.php
@@ -5,15 +5,13 @@ declare(strict_types=1);
 namespace Terpz710\EnderKits;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
-use Terpz710\EnderKits\Task\CoolDownTask;
 use Terpz710\EnderKits\Command\KitCommand;
+use Terpz710\EnderKits\Task\CoolDownTask;
 
-class Main extends PluginBase {
-
+class EnderKitsPlugin extends PluginBase {
+    
     public function onEnable(): void {
         $this->getServer()->getCommandMap()->register("kit", new KitCommand($this));
-        $this->getScheduler()->scheduleRepeatingTask(new CoolDownTask($this, new Config($this->getDataFolder() . "kits.yml", Config::YAML)), 20 * 60);
-        $this->saveResource("kits.yml");
+        $this->getScheduler()->scheduleRepeatingTask(new CoolDownTask($this), 20 * 60);
     }
 }
